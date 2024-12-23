e97855f4c586fb047c39184a418a8ff5b1ba6c73
Development 5 <35240334+therealnoskill@users.noreply.github.com>
2024-12-23 21:35:42 +0100

    release v 1.0

--- client.lua
+++ client.lua
@@ -1,12 +1,14 @@
+-- CLIENT-SIDE
 local QBCore = exports['qb-core']:GetCoreObject()
 
 -- Weihnachtsbaum-Prop-Model
 local treeModel = `prop_xmas_tree_int`
 local interactDistance = 2.0 -- Distanz, in der der Spieler interagieren kann
 local drawDistance = 10.0 -- Entfernungsbereich für das Zeichnen des Texts
-local harvesting = false
+local harvesting = false -- Status, ob aktuell ein Baum geplündert wird
 local harvestedTrees = {} -- Tabelle zum Speichern der geplünderten Baum-Positionen
 
+-- Wenn die Weihnachts-Wetteroption aktiviert ist, wird das Wetter und andere Effekte angepasst
 if Config.XmasWeather then
     SetWeatherTypeNowPersist('xmas')
     SetForceVehicleTrails(true)
@@ -19,36 +21,37 @@ local function findActiveTrees()
     local success
     repeat
         if GetEntityModel(object) == treeModel then
+            -- Koordinaten des gefundenen Objekts abrufen
             local treeCoords = GetEntityCoords(object)
-            -- Baum-Position als Key verwenden, um das Array zu markieren
-            if not harvestedTrees[tostring(treeCoords)] then
-                -- Speichern der Koordinaten als Key (String-Format)
-                harvestedTrees[tostring(treeCoords)] = {coords = treeCoords, isHarvested = false}
+            local treeKey = tostring(treeCoords) -- Konvertieren der Koordinaten in einen eindeutigen Schlüssel
+            -- Baum nur hinzufügen, wenn er noch nicht bekannt ist und nicht geplündert wurde
+            if not harvestedTrees[treeKey] then
+                harvestedTrees[treeKey] = {coords = vector3(treeCoords.x, treeCoords.y, treeCoords.z), isHarvested = false}
             end
         end
-        success, object = FindNextObject(handle)
+        success, object = FindNextObject(handle) -- Nächstes Objekt finden
     until not success
-    EndFindObject(handle)
+    EndFindObject(handle) -- Suche beenden
 end
 
 -- Funktion zum Abspielen der Ernteanimation
 local function playHarvestAnimation()
     local playerPed = PlayerPedId()
-    local animDict = "amb@prop_human_bum_bin@idle_b"  -- Beispiel-Animation
+    local animDict = "amb@prop_human_bum_bin@idle_b"  -- Animation: Beispiel
     local animName = "idle_d"
 
-    -- Animation laden und abspielen
+    -- Animationsdatei laden
     RequestAnimDict(animDict)
     while not HasAnimDictLoaded(animDict) do
-        Wait(0)
+        Wait(0) -- Warten, bis die Animation geladen ist
     end
+    -- Animation abspielen
     TaskPlayAnim(playerPed, animDict, animName, 8.0, 8.0, 5000, 1, 0, false, false, false)
-
     Wait(5000) -- Warten, bis die Animation endet
-    ClearPedTasks(playerPed)
+    ClearPedTasks(playerPed) -- Animation abbrechen
 end
 
--- Text beim Interagieren anzeigen
+-- Funktion zum Anzeigen von 3D-Text im Spiel
 local function Draw3DText(x, y, z, text)
     SetTextScale(0.35, 0.35)
     SetTextFont(4)
@@ -62,124 +65,73 @@ local function Draw3DText(x, y, z, text)
     ClearDrawOrigin()
 end
 
--- Interaktion mit Weihnachtsbäumen
-CreateThread(function()
-    while true do
-        local sleep = 1000
-        local playerCoords = GetEntityCoords(PlayerPedId())
+-- Event: Spieler betritt den Server
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    print('Im a client and I just loaded into your server! noskill_christmas')
 
-        -- Alle Bäume durchgehen
-        for _, tree in pairs(harvestedTrees) do
-            local treeCoords = tree.coords -- Verwende den gespeicherten `coords`-Wert
-            local distance = #(playerCoords - treeCoords)
-            
-            -- Wenn Baum in Interaktionsbereich
-            if distance < drawDistance then
-                sleep = 0
+    -- Thread zur Interaktion mit Weihnachtsbäumen
+    CreateThread(function()
+        while true do
+            local sleep = 1000 -- Standardmäßig lange Wartezeit
+            local playerCoords = GetEntityCoords(PlayerPedId()) -- Spielerposition abrufen
 
-                if not tree.isHarvested then
-                    -- Baum ist noch nicht geplündert
-                    Draw3DText(treeCoords.x, treeCoords.y, treeCoords.z + 1.5, "[E] Um zu plündern")
+            for _, tree in pairs(harvestedTrees) do
+                -- Konvertieren der gespeicherten Koordinaten in einen Vector3
+                local treeCoords = vector3(tree.coords.x, tree.coords.y, tree.coords.z)
+                local distance = #(playerCoords - treeCoords) -- Abstand zwischen Spieler und Baum
 
-                    -- Interaktionsabfrage bei sehr naher Entfernung
-                    if distance < interactDistance and IsControlJustPressed(0, 38) and not harvesting then
-                        harvesting = true
-                        playHarvestAnimation()  -- Animation abspielen
+                if distance < drawDistance then
+                    sleep = 0 -- Reduzieren der Wartezeit, wenn ein Baum in der Nähe ist
 
-                        -- Markiere den Baum als geplündert
-                        tree.isHarvested = true
-                        harvestedTrees[tostring(treeCoords)] = tree
+                    if not tree.isHarvested then
+                        -- Interaktionstext anzeigen
+                        Draw3DText(treeCoords.x, treeCoords.y, treeCoords.z + 1.5, "[E] Um zu plündern")
 
-                        TriggerServerEvent('christmas_trees:harvestTree', treeCoords)
+                        if distance < interactDistance and IsControlJustPressed(0, 38) and not harvesting then
+                            harvesting = true -- Interaktionsstatus setzen
+                            playHarvestAnimation() -- Animation abspielen
+                            tree.isHarvested = true -- Baum als geplündert markieren
+                            harvestedTrees[tostring(treeCoords)] = tree
 
-                        -- Lösche den Baum, wenn Config.DeleteTrees aktiv ist
-                        if Config.DeleteTrees then
-                            Wait(2000)  -- Warte 2 Sekunden
-                            DeleteObject(tree) -- Lösche das Baum-Objekt
-                            harvestedTrees[tostring(treeCoords)] = nil  -- Entferne Baum aus der Liste
+                            -- Synchronisierung über Server
+                            TriggerServerEvent('christmas_trees:harvestTree', treeCoords)
+                            harvesting = false -- Interaktionsstatus zurücksetzen
                         end
-
-                        harvesting = false
+                    else
+                        -- Hinweistext für bereits geplünderte Bäume anzeigen
+                        Draw3DText(treeCoords.x, treeCoords.y, treeCoords.z + 1.5, "Dieser Baum wurde bereits geplündert")
                     end
-                else
-                    -- Baum wurde bereits geplündert, zeige eine Nachricht an
-                    Draw3DText(treeCoords.x, treeCoords.y, treeCoords.z + 1.5, "Dieser Baum wurde bereits geplündert")
                 end
             end
-        end
 
-        Wait(sleep)
-    end
+            Wait(sleep) -- Warten, um Ressourcen zu schonen
+        end
+    end)
+
+    -- Thread zum regelmäßigen Scannen der Welt nach Bäumen
+    CreateThread(function()
+        while true do
+            findActiveTrees()
+            Wait(10000) -- Alle 10 Sekunden scannen
+        end
+    end)
 end)
 
--- Scannt alle Objekte regelmäßig nach Weihnachtsbäumen
+-- Initiale Synchronisierung der geplünderten Bäume mit dem Server
 CreateThread(function()
-    while true do
-        findActiveTrees()  -- Aktualisiere die Liste der Weihnachtsbäume
-        Wait(10000)  -- Alle 10 Sekunden nach neuen Bäumen suchen
-    end
+    Wait(1000) -- Kurze Verzögerung zur Sicherheit
+    QBCore.Functions.TriggerCallback('christmas_trees:getHarvestedTrees', function(serverTrees)
+        for treeKey, data in pairs(serverTrees) do
+            harvestedTrees[treeKey] = {coords = vector3(data.coords.x, data.coords.y, data.coords.z), isHarvested = data.isHarvested}
+        end
+    end)
 end)
 
-Citizen.CreateThread(function()
-    
-    local showHelp = true
-    local loaded = false
-    
-    while true do
-        if enableWeatherControl then
-            SetWeatherTypeNowPersist('XMAS')
-        end
-        Citizen.Wait(0) -- prevent crashing
-        if IsNextWeatherType('XMAS') then -- check for xmas weather type
-            -- enable frozen water effect (water isn't actually ice, just looks like there's an ice layer on top of the water)
-            N_0xc54a08c85ae4d410(3.0)
-            -- preview: https://vespura.com/hi/i/2eb901ad4b1.gif
-            
-            SetForceVehicleTrails(true)
-            SetForcePedFootstepsTracks(true)
-            
-            if not loaded then
-                RequestScriptAudioBank("ICE_FOOTSTEPS", false)
-                RequestScriptAudioBank("SNOW_FOOTSTEPS", false)
-                RequestNamedPtfxAsset("core_snow")
-                while not HasNamedPtfxAssetLoaded("core_snow") do
-                    Citizen.Wait(0)
-                end
-                UseParticleFxAssetNextCall("core_snow")
-                loaded = true
-            end
-            RequestAnimDict('anim@mp_snowball') -- pre-load the animation
-            if IsControlJustReleased(0, 119) and not IsPedInAnyVehicle(GetPlayerPed(-1), true) and not IsPlayerFreeAiming(PlayerId()) and not IsPedSwimming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) and GetInteriorFromEntity(PlayerPedId()) == 0 and not IsPedShooting(PlayerPedId()) and not IsPedUsingAnyScenario(PlayerPedId()) and not IsPedInCover(PlayerPedId(), 0) then -- check if the snowball should be picked up
-                TaskPlayAnim(PlayerPedId(), 'anim@mp_snowball', 'pickup_snowball', 8.0, -1, -1, 0, 1, 0, 0, 0) -- pickup the snowball
-                Citizen.Wait(1950) -- wait 1.95 seconds to prevent spam clicking and getting a lot of snowballs without waiting for animatin to finish.
-                GiveWeaponToPed(GetPlayerPed(-1), GetHashKey('WEAPON_SNOWBALL'), 1, false, true) -- get 2 snowballs each time.
-            end
-            if not IsPedInAnyVehicle(GetPlayerPed(-1), true) --[[and not IsPlayerFreeAiming(PlayerId())]] then
-                if showHelp then
-                    showHelpNotification()
-                end
-                showHelp = false
-            else
-                showHelp = true
-            end
-        else
-            -- disable frozen water effect
-            if loaded then N_0xc54a08c85ae4d410(0.0) end
-            loaded = false
-            RemoveNamedPtfxAsset("core_snow")
-            ReleaseNamedScriptAudioBank("ICE_FOOTSTEPS")
-            ReleaseNamedScriptAudioBank("SNOW_FOOTSTEPS")
-            SetForceVehicleTrails(false)
-            SetForcePedFootstepsTracks(false)
-        end
-        if GetSelectedPedWeapon(PlayerPedId()) == GetHashKey('WEAPON_SNOWBALL') then
-            SetPlayerWeaponDamageModifier(PlayerId(), 0.0)
-        end
+-- Event: Aktualisierung eines Baumes durch den Server
+RegisterNetEvent('christmas_trees:updateTreeStatus', function(coords)
+    print("Status von Baum auf Client aktualisiert: ", coords)
+    local treeKey = tostring(coords)
+    if harvestedTrees[treeKey] then
+        harvestedTrees[treeKey].isHarvested = true -- Baum als geplündert markieren
     end
 end)
-
-function showHelpNotification()
-    BeginTextCommandDisplayHelp("STRING")
-    AddTextComponentSubstringPlayerName("Drücke ~INPUT_VEH_FLY_VERTICAL_FLIGHT_MODE~, während du zu Fuß bist, um Schneebälle aufzuheben!")
-    EndTextCommandDisplayHelp(0, 0, 1, -1)
-end
--- server.lua
+++ server.lua
@@ -1,19 +1,18 @@
+-- SERVER-SIDE
 local QBCore = exports['qb-core']:GetCoreObject()
 
 -- Belohnungen
 local rewards = {
     {item = "weihnachtsmann", label = "Weihnachtsmann", chance = 85}, 
-    {item = "zuckerstangen", label = "Zuckerstangen", chance = 65}, 
-    {item = "zuckerwatte", label = "Zuckerwatte", chance = 63}, 
-    {item = "mutzen", label = "Mutzen", chance = 61}, 
-    {item = "gluehwein", label = "Gluehwein", chance = 69}, 
+    {item = "zuckerstangen", label = "Zuckerstangen", chance = 55}, 
+    {item = "zuckerwatte", label = "Zuckerwatte", chance = 40}, 
+    {item = "mutzen", label = "Mutzen", chance = 35}, 
+    {item = "gluehwein", label = "Gluehwein", chance = 35}, 
     {item = "lebkuchenherz", label = "Lebkuchenherz", chance = 57}, 
-    {item = "krakauer", label = "Krakauer", chance = 35}, 
     {item = "champinons", label = "Champinons", chance = 25}, 
     {item = "phone", label = "Smartphone", chance = 6}, 
     {item = "tablet", label = "Tablet", chance = 4}, 
     {item = "digitalwaage", label = "Feinwaage", chance = 20}, 
-    {item = "xtcbaggy", label = "XTC-Baggy", chance = 15}, 
     {item = "packaged_dosidos", label = "Dosidos-Packung", chance = 20}, 
     {item = "trojan_usb", label = "Trojan USB", chance = 10}, 
     {item = "advancedrepairkit", label = "Erweitertes Reparaturkit", chance = 40}, 
@@ -22,12 +21,12 @@ local rewards = {
     {item = "heavyarmor", label = "Schwere Rüstung", chance = 5}, 
     {item = "powerbank", label = "Powerbank", chance = 25}, 
     {item = "noskill_repairkit", label = "Standard-Reparaturkit", chance = 15}, 
-    {item = "wheel_ticket", label = "Goldenes Ticket", chance = 2}, 
-    {item = "ayran", label = "Ayran", chance = 24}, 
+    {item = "wheel_ticket", label = "Goldenes Ticket", chance = 4}, 
     {item = "kq_winch", label = "Seilwinde", chance = 12}, 
-    {item = "meth_bag", label = "Meth-Beutel", chance = 15}, 
     {item = "random_keys", label = "Zufälliger Fahrzeugschlüssel", chance = 1}, 
 }
+
+local harvestedTrees = {} -- Speichert geplünderte Baum-Positionen
 
 -- Hilfsfunktion zur gewichteten Auswahl
 local function getRandomReward()
@@ -47,11 +46,22 @@ local function getRandomReward()
     end
 end
 
+-- Event: Baum plündern
 RegisterNetEvent('christmas_trees:harvestTree', function(coords)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-
     if not Player then return end
+
+    local treeKey = tostring(coords)
+
+    -- Prüfen, ob der Baum bereits geplündert wurde
+    if harvestedTrees[treeKey] then
+        TriggerClientEvent('QBCore:Notify', src, "Dieser Baum wurde bereits geplündert.", "error")
+        return
+    end
+
+    -- Baum als geplündert markieren
+    harvestedTrees[treeKey] = {coords = coords, isHarvested = true}
 
     -- Belohnung auswählen
     local reward = getRandomReward()
@@ -61,4 +71,12 @@ RegisterNetEvent('christmas_trees:harvestTree', function(coords)
     else
         TriggerClientEvent('QBCore:Notify', src, "Du hast nichts gefunden. Versuch es nochmal!", "error")
     end
+
+    -- Synchronisation mit Clients
+    TriggerClientEvent('christmas_trees:updateTreeStatus', -1, coords)
+end)
+
+-- Callback für initiale Synchronisierung
+QBCore.Functions.CreateCallback('christmas_trees:getHarvestedTrees', function(source, cb)
+    cb(harvestedTrees)
 end)
